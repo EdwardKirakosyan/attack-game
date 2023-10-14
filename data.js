04b5f2a9bda2b103997a56364c34ede5e81d0779
Edward Kirakosyan <kirakosyan3010@gmail.com>
2023-10-15 01:16:14 +0400

    c

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 const characterData = {
   hero: {
-    name: "Robot",
+    name: "ROBOT",
     avatar: "images/robot.jpg",
     health: 60,
     diceCount: 3,
     currentDiceScore: [],
   },
   orc: {
-    name: "Orc",
+    name: "ORC",
     avatar: "images/orc.png",
     health: 30,
     diceCount: 1,
     currentDiceScore: [],
   },
   demon: {
-    name: "Demon",
+    name: "DEMON",
     avatar: "images/demon.png",
     health: 25,
     diceCount: 2,
     currentDiceScore: [],
   },
   goblin: {
-    name: "Goblin",
+    name: "GOBLIN",
     avatar: "images/goblin.png",
     health: 20,
     diceCount: 3,
